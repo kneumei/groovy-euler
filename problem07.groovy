8c55c1353e2d31d4a39a8715963e493b5e1cc81a
unknown <neumeierk@Neumeierk.foliofn.corp>
2012-08-29 23:32:08 -0400

    optimized isPrime() method based on analysis from project euler

--- problem07.groovy
+++ problem07.groovy
@@ -1,10 +1,19 @@
 def isPrime = { N->
-  if(N==2) return true;
-  for(i in 2..Math.sqrt(N)){
-    if(N%i==0)
-     return false;
+  if (N== 1)  return false;
+  if (N== 2)  return true;
+  if (N== 3)  return true;
+  if (N== 4)  return false;
+  if (N== 5)  return true;
+  if(N%2 == 0) return false;
+  if(N%3 == 0) return false;
+
+  //all primes >3 can be written in the form 6k +/- 1
+
+  for(int i=5; i<=Math.ceil(Math.sqrt(N)); i=i+6){
+    if(N % i==0) return false;
+    if(N % (i+2)==0) return false;
    }
-    return true;
+  return true;
 }
 
 println LazyList.integersStartingFrom(1).filter(isPrime).get(10000);
