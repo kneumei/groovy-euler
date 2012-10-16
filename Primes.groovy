3148902ab8c8e19f2a4b000b07c96c45a955c5ba
kneumei <kneumei@gmail.com>
2012-10-15 20:27:56 -0400

    Solved problem 10

---
+++ Primes.groovy
@@ -0,0 +1,26 @@
+class Primes{ 
+  static def isPrime = { N->
+    if (N== 1)  return false;
+    if (N== 2)  return true;
+    if (N== 3)  return true;
+    if (N== 4)  return false;
+    if (N== 5)  return true;
+    if(N%2 == 0) return false;
+    if(N%3 == 0) return false;
+
+    //all primes >3 can be written in the form 6k +/- 1
+
+    for(int i=5; i<=Math.ceil(Math.sqrt(N)); i=i+6){
+      if(N % i==0) return false;
+      if(N % (i+2)==0) return false;
+    }
+
+    //for(int i=5; i<=N/2; i=i+2){
+    //  if(N%i==0)return false;
+    //}
+    return true;
+  }
+
+}
+
+
---
+++ problem10.groovy
@@ -0,0 +1,17 @@
+/*
+* Problem 10
+* Find the sum of all the primes below two million.
+* Answer: 142913828922
+*
+* This answer generates all numbers up to two million, then screens each one
+* for being prime, then calculates the product.  The method to calculate 
+* isPrime() was used by a different problem.  One trick is that the accumulator
+* must be a long. 
+*
+* Another solution would be to use the Sieve of Eratosthenes to generate 
+* only prime numbers.
+*/
+
+def range = 1..2000000
+
+println range.findAll{n -> Primes.isPrime(n)}.inject(0l,{x,accum-> x+accum})
